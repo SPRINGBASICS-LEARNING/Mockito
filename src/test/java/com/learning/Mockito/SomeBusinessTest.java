16e83b4f45636b437f7c4fe7d5d0e5d5f1cd8982
Tamminedi <vtammine@iMac.local>
2020-11-14 15:23:29 -0500

    Using a stub - Disadvantages

---
+++ src/test/java/com/learning/Mockito/SomeBusinessTest.java
@@ -0,0 +1,23 @@
+package com.learning.Mockito;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeBusinessTest {
+
+    @Test
+    public void findTheGreatestFromAllData() {
+     SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub());
+     int result = businessImpl.findTheGreatestFromAllData();
+     assertEquals(24, result);
+    }
+
+    class DataServiceStub implements DataService {
+
+        @Override
+        public int[] retrieveAllData() {
+            return new int[] {24,6,15};
+        }
+    }
+}
